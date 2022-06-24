37d94a03dbb0b94e323c1d2eec1572c2bfd867ad
Ender <81829364+Endxca@users.noreply.github.com>
2022-06-25 00:24:53 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,20 +1,39 @@
-clear
 echo "[+] proxy checker by ender v1.2"
+clear
 read -p "proxy file : " proxy_file
 OLDIFS=$IFS
-works=()
-nope=()
 echo "[*] Testing proxys..."
-while IFS=$' \t\r\n' read -r line; do
-        test=$(timeout 2 curl -x "$line" http://1.1.1.1)
+proxys=()
+doit(){
+        prox=$1
+        test=$(curl -x "$prox" http://1.1.1.1 --silent --max-time 2)
         if [ "${#test}" -gt 0 ] ; then
-                works+=("$line")
+                echo "works : $prox"
         else
-                nope+=("$line")
+                nope+=("$prox")
+                echo "nope : $prox"
         fi
+}
+export -f doit
+
+while IFS=$' \t\r\n' read -r line; do
+proxys+=("$line")
 done < $proxy_file
+IFS=$'\n' read -r -d '' -a array < <(parallel -j 100 -k doit ::: "${proxys[@]}" && printf '\0')
 IFS=$OLDIFS
 clear
+works=()
+nope=()
+for  i in "${array[@]}" ; do
+        if grep -q "works :" <<< "$i" ; then
+                j="${i//works : /}"
+                works+=("$j")
+        else
+                j="${i//nope : /}"
+                nope+=("$j")
+        fi
+done
+
 echo "[+]${#works[@]} Proxy are working and ${#nope[@]} are Not"
 echo "[*] Writing all proxy to a file..."
 for i in "${works[@]}" ; do
